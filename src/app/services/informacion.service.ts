922a1a7b2f55749925acee3ab754fae9ef548bcf
Esteban Moya <moyaco41@gmail.com>
2017-12-14 23:48:43 -0500

    firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -4,15 +4,36 @@ import {Http} from '@angular/http';
 export class InformacionService {
 
   info: any = {};
-  cargada:boolean = false;
-  constructor(public http: Http) {
+  cargada = false;
+  cargada2= false;
+  equipo: any= {};
+    constructor(public http: Http) {
 
-      this.http.get('assets/data/info.pagina.json').subscribe(data => {
-        console.log(data.json());
-        this.cargada = true;
-        this.info = data.json();
-      });
+    this.carga_info();
+    this.carga_sobre_nosotros();
 
    }
+
+    public carga_info()
+   {
+
+    this.http.get('assets/data/info.pagina.json').subscribe(data => {
+
+              this.cargada = true;
+              this.info = data.json();
+            });
+   }
+
+    public carga_sobre_nosotros()
+    {
+
+      this.http.get('https://paginaweb-e539f.firebaseio.com/equipo.json').subscribe(data => {
+                      this.cargada2 = true;
+                      this.equipo = data.json();
+                      console.log(data.json());
+
+                    });
+
+    }
 
 }
