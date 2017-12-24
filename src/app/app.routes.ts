a825dde08b50a8f6c81c05d1b0fc6cfcc2289396
Esteban Moya <moyaco41@gmail.com>
2017-12-24 09:48:37 -0500

    Pagina de busqueda creada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
-import {AboutComponent, ItemComponent, PortafolioComponent} from './components/index.paginas';
+import {AboutComponent, ItemComponent, PortafolioComponent, SearchComponent} from './components/index.paginas';
 
 const routes: Routes = [
   { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'item/:id', component: ItemComponent },
+  { path: 'buscar/:termino', component: SearchComponent},
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
+
 ];
 
 export const appRouting = RouterModule.forRoot(routes, {useHash: true});
