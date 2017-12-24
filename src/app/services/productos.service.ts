e6ddf89bcd73626c732036e600db40075509be23
Esteban Moya <moyaco41@gmail.com>
2017-12-24 10:42:36 -0500

    V1.0

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,7 +5,7 @@ export class ProductosService {
 
   productos:any[] =[];
   cargando:boolean =true;
-
+  productos_filtrado:any[] = [];
   constructor(private http: Http) {
     
        this.cargarProductos();
@@ -14,18 +14,54 @@ export class ProductosService {
 
   public cargarProductos()
   {
-    if(this.productos.length ===0)
-    {
-      this.http.get('https://paginaweb-e539f.firebaseio.com/productos_idx.json').subscribe(res =>{
-        this.productos=res.json();
-        this.cargando=false;
-      });
-    }
+    let promesa=new Promise((resolve,reject)=>{
+
+      if(this.productos.length ===0)
+      {
+        this.http.get('https://paginaweb-e539f.firebaseio.com/productos_idx.json').subscribe(res =>{
+          this.productos=res.json();
+          this.cargando=false;
+          resolve();
+        });
+      }
+
+    });
+    return promesa;
   }
 
   public cargar_producto(id:string)
   {
     return this.http.get('https://paginaweb-e539f.firebaseio.com/productos/'+id+'.json');
+  }
+
+  public buscar_producto(termino:string)
+  {
+    if(this.productos.length===0)
+    {
+        this.cargarProductos().then( ()=>{
+        this.filtrar_productos(termino);
+        });
+    }else{
+      this.filtrar_productos(termino);
+    }
+
+
+   
+  }
+
+  private filtrar_productos(termino:string)
+  {
+    this.productos_filtrado=[];
+    termino=termino.toLowerCase();
+    this.productos.forEach( prod=>{
+      
+            if(prod.categoria.indexOf(termino)>=0|| prod.titulo.toLowerCase().indexOf(termino)>=0)
+            {
+              this.productos_filtrado.push(prod);
+            }
+            
+      
+          });
   }
 
 }
